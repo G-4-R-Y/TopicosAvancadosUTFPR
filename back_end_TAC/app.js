eec8b0b8589105d8856acacbbab555c8b5344fcf
G-4-R-Y <rcf.jr@hotmail.com>
2021-12-14 23:36:38 -0300

    cors_7

--- back_end_TAC/app.js
+++ back_end_TAC/app.js
@@ -17,10 +17,10 @@ var app = express();
 //    origin: "*"
 //}));
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });  
+});  
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
